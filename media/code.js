61940b4cd8d42692917991cdd9a660d71ab619eb
hyperClink <45831360+hyperClink@users.noreply.github.com>
2019-01-31 23:46:15 +0300

    hitboxes, better controls

--- media/code.js
+++ media/code.js
@@ -14,6 +14,8 @@
 	xPos=innerWidth*0.08,
 	yPos=0,
 	xOffset = 0,
+	direction = 'right',
+	att_dir = 1;
 
 	//player controls
 	left = false,
@@ -33,8 +35,8 @@
 
 
 	//player animation
-	player_animation = [{height:0, frames:0, state:null}],
-	player_strg = [{height:0, frames:0, state:null}],
+	player_animation = [{height:0, frames:0, state:null, x_offset_:0}],
+	player_strg = [{height:0, frames:0, state:null, x_offset_:0}],
 	player_width = 0,
 	n = 0,
 	player_speed = 4.6,
@@ -64,76 +66,54 @@ enemy_zombie.src = 'character animation/enemies/zomb_0.png';
 player_animation.height = 334.75;
 player_animation.frames = 17;
 player_animation.state = player_idle;
+player_animation.x_offset_ = 0;
 
 document.addEventListener('keydown', js_event);
 document.addEventListener('keyup', keyup_event);
 
 function js_event(event){
+if (attack==false){
 
-	//literally walk
-if (event.keyCode==65 || event.keyCode==68){
-	player_animation.state = player_walk;
-	player_animation.height = 323.25;
-	player_animation.frames = 24;
-};
+//turning and attack
 
-switch (event.keyCode) {
+	switch (event.keyCode) {
 
-	case 65:
-		if (attack==false){
-			left = true;
+		case 65:
+				left = true;
+			break;
+
+		case 68:
+				right = true;
+			break;
+
+			case 81:
+
+				player_animation.x_offset_ = -26;
+				player_animation.state = player_attack;
+				player_animation.height = 331;
+				player_animation.frames = 20;
+
+				n = 0;
+				attack = true;
+
+				break;
+
+			default:
+				console.log(event.keyCode);
+				break;
+
 		};
-		break;
-
-	case 68:
-		if (attack==false){
-			right = true;
-		};
-		break;
-
-	case 81:
-
-		if (attack == false){
-
-			//store previous variables
-			player_strg.state = player_animation.state;
-			player_strg.height = player_animation.height;
-			player_strg.frames = player_animation.frames;
-
-			n = 0;
-			attack = true;
-		};
-
-		break;
-
-	default:
-		console.log(event.keyCode);
-		break;
-
 };
 
 };
 
 function keyup_event(event){
-	if (event.keyCode==65 || event.keyCode==68){
-
-		if(attack==false){
-			n=0;
-		};
-
-		player_animation.state = player_idle;
-		player_animation.height = 334.75;
-		player_animation.frames = 17;
-		scroll = false;
-
-		//modify storage during animation
-		player_strg.state = player_animation.state;
-		player_strg.height = player_animation.height;
-		player_strg.frames = player_animation.frames;
+	if((event.keyCode==65 || event.keyCode==68) && attack==false){
+		n=0;
 	};
 
-
 switch (event.keyCode) {
+
 	case 65:
 		left = false;
 		break;
@@ -181,34 +161,41 @@ function timer(){
 //player
 function player(xPos, yPos){
 
-	if(right==true){
+	if(direction=='right'){
 		player_width=0;
-		xOffset = 0;
+		xOffset = 0-player_animation.x_offset_;
+		att_dir = 1;
 	};
 
-	if(left==true){
+	if(direction=='left'){
 		player_width=491.25;
-		xOffset = 151;
+		xOffset = 151+player_animation.x_offset_;
+		att_dir = -1;
 	};
 
-	if (attack==true){
-		player_animation.state = player_attack;
-		player_animation.height = 331;
-		player_animation.frames = 20;
-		xOffset=32;
+	if (attack==false){
+		if( (left == true || right == true) != (left == true && right == true) ){
+			player_animation.state = player_walk;
+			player_animation.height = 323.25;
+			player_animation.frames = 24;
+		}else{
+			player_animation.state = player_idle;
+			player_animation.height = 334.75;
+			player_animation.frames = 17;
+			scroll = false;
+		};
 	};
 
-	ctx.drawImage(player_animation.state, player_width, player_animation.height*n, 491.25, 323, xPos-xOffset, innerHeight-yPos-190, 220+window_size, 130+window_size);
+//player anims
+	ctx.drawImage(player_animation.state, player_width, player_animation.height*n, 491.25, 323, xPos-xOffset, innerHeight-yPos-200, 220+window_size, 130+window_size);
+
 //reset anim
 	if (n>=player_animation.frames) {
 		n=0;
 
 		if(attack==true){
 			attack=false;
-
-			player_animation.state = player_strg.state;
-			player_animation.height = player_strg.height;
-			player_animation.frames = player_strg.frames;
+			player_animation.x_offset_=0;
 		};
 
 	}else{
@@ -255,15 +242,15 @@ function zombie(xPosd1, yPosd1){
 
 
 //spawn enemies
-function spawnEnemies(name, xPos, yPos, height, width, frames, animSpeed, horiz, vertical, n, xScale, yScale, speed){
+function spawnEnemies(name, xPos, yPos, height, width, frames, animSpeed, horiz, vertical, n, xScale, yScale, speed, direction, health){
 	enemies.push({name, xPos, yPos,
-		height, width, frames, animSpeed, horiz, vertical, n, xScale, yScale, speed});
+		height, width, frames, animSpeed, horiz, vertical, n, xScale, yScale, speed, direction, health});
 };
 
 function spawnLogic(){
-	//name, xPos, yPos, height, width, frames, animSpeed, horiz, vertical, n, xScale, yScale, speed
-	spawnEnemies(dog_sp, randomInt(0, innerWidth), 120, 85.25, 125, 7, 1, 0, 1, 0, 100, 80, 12);
-	setTimeout(spawnLogic, randomInt(10000, 15000));
+	//name, xPos, yPos, height, width, frames, animSpeed, horiz, vertical, n, xScale, yScale, speed, dir, health
+	spawnEnemies(dog_sp, innerWidth/2-((innerWidth/2.1)*flip()), 120, 85.25, 125, 7, 1, 0, 1, 0, 100, 80, 12, flip(), 10);
+	setTimeout(spawnLogic, randomInt(2000, 3000));
 };
 
 //random integer
@@ -271,6 +258,10 @@ function randomInt(min, max){
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+function flip(){
+	return Math.round(Math.random())*2-1;
 };
 
 function main (){
@@ -286,7 +277,20 @@ function main (){
 			enemies[i].height*(Math.round(enemies[i].n)*enemies[i].vertical), enemies[i].width, enemies[i].height,
 			enemies[i].xPos, innerHeight-enemies[i].yPos, enemies[i].xScale, enemies[i].yScale);
 
-			enemies[i].xPos-=enemies[i].speed;
+			enemies[i].xPos+=(enemies[i].speed*att_dir);
+
+			ctx.fillRect(xPos+54+(-27*att_dir), 100, 10, 10);
+			ctx.fillRect((xPos+54+(-27*att_dir))+10*n*att_dir, 120, 10, 10);
+
+			ctx.fillRect(enemies[i].xPos, 10, 1, 10);
+			ctx.fillRect(enemies[i].xPos+enemies[i].width, 10, 1, 10);
+
+			ctx.fillText(enemies[i].health, enemies[i].xPos, innerHeight-20-enemies[i].yPos);
+
+			if ( (enemies[i].xPos+enemies[i].width>( xPos+54+(-27*att_dir) ) && enemies[i].xPos<( xPos+54+(-27*att_dir) ) + 8.5*n*att_dir+20) && attack==true){
+				enemies[i].xPos+=30*(-enemies[i].direction);
+				enemies[i].health--;
+			};
 
 			if (scroll == true){
 
@@ -308,27 +312,30 @@ function main (){
 			};
 
 //despawn enemy
-			if (enemies[i].xPos+enemies[i].name.width<=0){
+			if (enemies[i].xPos+enemies[i].name.width<=0 || enemies[i].xPos>=innerWidth || enemies[i].health<=0){
+				if(enemies[i].health<=0){
+					killed++;
+				}
 				enemies.splice(i--, 1);
 			};
-
-
-
 		};
 
 	};
 
+	//hitbox
+	ctx.fillRect(xPos+54+(-27*att_dir), innerHeight-yPos-110, 8.5*n*att_dir+20 , 1);
+
 	//health
 	ctx.fillStyle = '#FF0000';
-	ctx.fillRect(20, 20, health/0.5, 18);
+	ctx.fillRect(20, 28, health/0.5, 18);
 
 	//text
 	//health
 	ctx.font = '20px Georgia';
 	ctx.fillStyle = '#FFFFFF';
-	ctx.fillText(health, health-4, 34);
+	ctx.fillText(health, health-4, 41);
 
-	//controls
+	//controls - movement
 	if (left==true && attack==false){
 		if(xPos>innerWidth/2-(120+window_size)-50 || x_scroll<=0){
 			xPos-=player_speed;
@@ -349,12 +356,20 @@ function main (){
 		};
 	};
 
+	if(left == true && right != true){
+		direction='left'
+	}
+
+	if(right == true && left != true){
+		direction='right'
+	}
+
 	//timer tex t asdasd
 	ctx.font = '18px Georgia';
 	ctx.fillStyle = '#FFFFFF';
+	ctx.fillText('Username', 18, 20);
 	ctx.fillText('Time:', 10, innerHeight-35);
 	ctx.fillText(`${timeM}:${(getSecondsToday()-time)}`, 60, innerHeight-35);
-
 	ctx.fillText(`Killed: ${killed}`, 10, innerHeight-55);
 
 };
